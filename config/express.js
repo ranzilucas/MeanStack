9ddc8c7506dcbd8df443e8392c99b32457008594
U-DBSERVER\lucasr <lucasr@dbs-1433.dbserver.com.br>
2016-10-13 11:57:49 -0300

    adicionando configuracoes

--- config/express.js
+++ config/express.js
@@ -1,18 +1,25 @@
 //config express
 var express = require('express');
-module.exports = function(){
-    var app = express();
-    var home = require('./../app/routes/home');
+var load = require('express-load');
+var bodyParser = require('body-parser');
 
-    //variaveis de ambiente
-    app.set('port', 3000);
-    //middleware
-    app.use(express.static('./public'));
+module.exports = function() {
+  var app = express();
 
-    app.set('view engine', 'ejs');
-    app.set('views', './app/views');
+  app.set('port', 3001);
 
-    home(app);
+  app.use(express.static('./public'));
+  app.set('view engine', 'ejs');
+  app.set('views','./app/views');
+  // novos middlewares
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(require('method-override')())
 
-    return app;
-}
+  load('models', {cwd: 'app'})
+   .then('controllers')
+   .then('routes')
+   .into(app);
+
+  return app;
+};
